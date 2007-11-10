2667188bfdc7b234bb09388ad98639de8cde455e
Christoffer Kugg <kugghjul@gmail.com>
2007-11-10 13:41:40 +0000

    I made a oopsie witht he search and replce script. Redoing...

--- kugg/bugfix_444154/adress_replace.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for f in `find  -type f`;
- do
-  if grep -q "\#\ 59\ Temple\ Place\ -\ Suite\ 330,\ Boston,\ MA\ 02111-1307,\ USA." $f;
-  then
-   sed -e s/\#\ 59\ Temple\ Place\ -\ Suite\ 330,\ Boston,\ MA\ 02111-1307,\ USA./\#\ 51\ Franklin\ Street,\ Fifth\ Floor,\ Boston,\ MA\ 02110-1301,\ USA/g fai-gui/repo/svn/fai/people/kugg/bugfix_444154/
-  fi
-done

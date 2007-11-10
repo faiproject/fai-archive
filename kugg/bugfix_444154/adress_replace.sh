dc5ebb3848a4d4e505dce8a384be13a846cd8d0f
Christoffer Kugg <kugghjul@gmail.com>
2007-11-10 13:59:00 +0000

    Just checked in to be able to remove this trunk copy in next commit. *Stupid*

--- kugg/bugfix_444154/adress_replace.sh
+++ kugg/bugfix_444154/adress_replace.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for f in `find  -type f`| grep -v ".svn" ;
+for f in `find . -type f`| grep -v ".svn" | grep -v adress_replace.sh ;
  do
   if grep -q "\#\ 59\ Temple\ Place\ -\ Suite\ 330,\ Boston,\ MA\ 02111-1307,\ USA." $f;
   then
    cp $f $f.bak
    sed -e s/\#\ 59\ Temple\ Place\ -\ Suite\ 330,\ Boston,\ MA\ 02111-1307,\ USA./\#\ 51\ Franklin\ Street,\ Fifth\ Floor,\ Boston,\ MA\ 02110-1301,\ USA/g < $f.bak > $f 
    rm $f.bak
-   fi
+  fi
 done

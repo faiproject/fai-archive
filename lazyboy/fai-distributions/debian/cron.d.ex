bff4b51e9bcafe9b4c33bd0346bcf4eeab3dcfa1
Thomas Lange <lange@informatik.uni-koeln.de>
2011-04-15 11:13:11 +0000

    remove obsolete branch

--- lazyboy/fai-distributions/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the fai-distributions package
-#
-0 4	* * *	root	fai-distributions_maintenance
--- lazyboy/fai-distributions/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(fai-distributions):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="fai-distributions" command="/usr/bin/fai-distributions"
